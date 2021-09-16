3563cd9fe7b7f473a5283bc73613a95da9bf1f52
Stuart Pomeroy <stuart@pomeroys.site>
2021-09-16 02:56:09 +0100

    Make sign links use books

--- src/main/java/dev/imabad/mceventsuite/spigot/commands/SignLinkCommand.java
+++ src/main/java/dev/imabad/mceventsuite/spigot/commands/SignLinkCommand.java
@@ -1,11 +1,15 @@
 package dev.imabad.mceventsuite.spigot.commands;
 
+import net.kyori.adventure.inventory.Book;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -20,11 +24,13 @@ public class SignLinkCommand extends BaseCommand{
     public boolean execute(@NotNull CommandSender sender, @NotNull String label, @NotNull String[] args) {
         String link = args[0];
         String text = getLastArgs(args, 1);
+        Player player = (Player) sender;
         BaseComponent[] textComponent = TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', text));
         for (BaseComponent baseComponent : textComponent) {
             baseComponent.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link));
         }
-        sender.sendMessage(textComponent);
+        player.openBook(Book.builder().addPage(
+                BungeeComponentSerializer.get().deserialize(textComponent)).author(Component.text("Cubed! 2021")).build());
         return true;
     }
 
