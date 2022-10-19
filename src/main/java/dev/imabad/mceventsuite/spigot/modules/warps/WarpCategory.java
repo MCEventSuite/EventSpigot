e69bf160ad52d61406323a079c237111ac865801
Paul19988 <paul@paulcodes.co.uk>
2022-10-19 01:35:50 +0100

    append larges onto new line

--- src/main/java/dev/imabad/mceventsuite/spigot/modules/warps/WarpCategory.java
+++ src/main/java/dev/imabad/mceventsuite/spigot/modules/warps/WarpCategory.java
@@ -7,8 +7,8 @@ import org.bukkit.inventory.ItemStack;
 public enum WarpCategory {
     ALL("", ItemUtils.createItemStack(Material.WHITE_CONCRETE, "&l&fAll"), 0),
     HALL("halls",ItemUtils.createItemStack(Material.RED_CONCRETE, "&l&bHalls"), Material.RED_CONCRETE, 0),
-    LARGE("large", ItemUtils.createItemStack(Material.CYAN_CONCRETE, "&l&bLarge Booths"), Material.LIGHT_BLUE_CONCRETE, 0),
-    MEDIUM("medium", ItemUtils.createItemStack(Material.YELLOW_CONCRETE, "&l&eMedium Booths"), Material.YELLOW_CONCRETE, 1),
+    LARGE("large", ItemUtils.createItemStack(Material.CYAN_CONCRETE, "&l&bLarge Booths"), Material.LIGHT_BLUE_CONCRETE, 1),
+    MEDIUM("medium", ItemUtils.createItemStack(Material.YELLOW_CONCRETE, "&l&eMedium Booths"), Material.YELLOW_CONCRETE, 2),
     SMALL("small", ItemUtils.createItemStack(Material.GREEN_CONCRETE, "&l&aSmall Booths"), Material.GREEN_CONCRETE, 3),
     OTHER("other", ItemUtils.createItemStack(Material.GRAY_CONCRETE, "&l&9Other"), 5);
 
